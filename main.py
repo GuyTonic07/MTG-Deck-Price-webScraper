ea2e9624f9fa4dc604601e03fcfcf123e6433e09
GuyTonic07 <corey.low2@gmail.com>
2024-07-22 19:38:16 -0500

    Update main.py
    Final Version

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Made by GuyTonic 
+#Made by GuyTonic/Corey 
 from discord import Intents, Message
 import discord
 from discord.ext import commands
@@ -6,48 +6,63 @@ from dotenv import load_dotenv, find_dotenv
 import os
 from typing import Final
 from Responses import get_response, get_deck_priceArch, get_deck_price_tapped
+from Sel import get_deck_price_moxfield
 
-load_dotenv(find_dotenv())
+
+load_dotenv(find_dotenv()) #This is where the discord Token his hidden yall can't have this
 TOKEN: Final = os.getenv('DISCORD_TOKEN')
 
-intents = discord.Intents.default()
+
+intents = discord.Intents.default() 
 intents.message_content = True
 
-#######################bot function#########################################
+#########################################bot function#########################################
 
 bot = commands.Bot(command_prefix="-", intents=intents, case_insensitive=True)
 
 
-@bot.event
+@bot.event # on bot start up
 async def on_ready():
-    channel = bot.get_channel(1222252767046013079)#bot testing channel
+    channel = bot.get_channel(1222252767046013079) #bot testing channel
     print(f"Logged in as {bot.user}")
     await channel.send('Hello world ') 
 
-@bot.command()
+@bot.command() #Shut down command
 @commands.is_owner()
 async def shutdown(ctx):
-    channel = bot.get_channel(1222252767046013079)#bot testing channel
+    channel = bot.get_channel(1222252767046013079) #bot testing channel
     await channel.send('logging off')
     await bot.close()
         
-@bot.command()
+@bot.command() #for the !quote command
 async def Quote(ctx):
     response = get_response()
     await ctx.send(response)
 
-@bot.command()
+@bot.command() #this set of commands control the webscrapper 
 async def pricecheck(ctx, decklink: str):
-    if 'archidekt.com' in decklink:
-        price = get_deck_priceArch(decklink)  # Call the function from
+    if 'scryfall.com' in decklink: #Scryfall NOT YET SUPPORTED
+        await ctx.send('I do not Support this yet, please ask <@645314806051766272>') #owners ID
+
+async def pricecheck(ctx, decklink: str):    
+    if 'deckstats.net' in decklink: #DeckStats NOT YET SUPPORTED 
+        await ctx.send('I do not Support this yet, please ask <@645314806051766272>')#owners ID
+    
+    elif 'moxfield.com' in decklink: #MoxField SUPPORTED
+        price = get_deck_price_moxfield(decklink) #Calls to Sel Function
+        if price:
+            await ctx.send(f'Based on card kingdom \nThe price of the deck is: {price}$')
+    
+    elif 'archidekt.com' in decklink: #Archidekt SUPPORTED 
+        price = get_deck_priceArch(decklink)  #Calls to Soup Function
         if price:
             await ctx.send(f'Based on Card kingdom \nThe price of the deck is: {price}$')
 
         else:
             await ctx.send('Failed to retrieve the deck price from Archidekt. Please check the link and try again.')
         
-    elif 'tappedout.net' in decklink:
-        prices = get_deck_price_tapped(decklink)
+    elif 'tappedout.net' in decklink: #Tappedout SUPPORTED
+        prices = get_deck_price_tapped(decklink) #Calls to soup Function
         
         prices1 = prices[0]
             
@@ -66,19 +81,19 @@ async def pricecheck(ctx, decklink: str):
 
 ###########################Error handling####################################
 @bot.event
-async def on_command_error(ctx, error):
+async def on_command_error(ctx, error): #this is the command to stop the bot from running only I (the owner) can excute this
     if isinstance(error, commands.CheckFailure) and ctx.command.name == 'shutdown':
         await ctx.send("You're not my owner!")
     else:
-        raise error  # Re-raise the error so the default handler can handle it
+        raise error  
 
 
 @bot.event
-async def on_command_error(ctx, error):
+async def on_command_error(ctx, error): #in the case of a price now coming back this will launch
     if isinstance(error, commands.CheckFailure) and ctx.command.name == 'pricecheck':
         await ctx.send("I cannot find the price.")
     else:
-        raise error  # Re-raise the error so the default handler can handle it
+        raise error 
     
     
 bot.run(TOKEN)
